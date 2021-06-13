d6affbc12e82cc28cd253a7bd036c34119db217c
CptSchnitz <12687466+CptSchnitz@users.noreply.github.com>
2021-06-13 17:19:35 +0300

    fix(configurations): fixed broken migrations

--- db/migrations/1623568810719-addFileIdConstraintToEntity.ts
+++ db/migrations/1623568810719-addFileIdConstraintToEntity.ts
@@ -4,12 +4,14 @@ export class addFileIdConstraintToEntity1623568810719 implements MigrationInterf
   name = 'addFileIdConstraintToEntity1623568810719';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "public"."entity" DROP CONSTRAINT "PK_83c6377c598f5fac5071ef7dacc"`);
-    await queryRunner.query(`ALTER TABLE "public"."entity" ADD CONSTRAINT "PK_b0d4cdc02ef316d72ae7ecf036a" PRIMARY KEY ("entity_id", "file_id")`);
+    await queryRunner.query(`ALTER TABLE "osm_sync_tracker"."entity" DROP CONSTRAINT "PK_d3ba6c77bfcc4d2bf9a20d64897"`);
+    await queryRunner.query(
+      `ALTER TABLE "osm_sync_tracker"."entity" ADD CONSTRAINT "PK_b0d4cdc02ef316d72ae7ecf036a" PRIMARY KEY ("entity_id", "file_id")`
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "public"."entity" DROP CONSTRAINT "PK_b0d4cdc02ef316d72ae7ecf036a"`);
-    await queryRunner.query(`ALTER TABLE "public"."entity" ADD CONSTRAINT "PK_83c6377c598f5fac5071ef7dacc" PRIMARY KEY ("entity_id")`);
+    await queryRunner.query(`ALTER TABLE "osm_sync_tracker"."entity" DROP CONSTRAINT "PK_b0d4cdc02ef316d72ae7ecf036a"`);
+    await queryRunner.query(`ALTER TABLE "osm_sync_tracker"."entity" ADD CONSTRAINT "PK_d3ba6c77bfcc4d2bf9a20d64897" PRIMARY KEY ("entity_id")`);
   }
 }
