eaad86a98dffa360bc7292ade4e7bb823144d1f2
amito <samit2310@gmail.com>
2021-08-18 15:19:34 +0300

    fix(file): added eslint ignore

--- tests/jest.d.ts
+++ tests/jest.d.ts
@@ -1,5 +1,6 @@
 declare global {
   namespace jest {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     interface Matchers<R> {
       toHaveStatus: (status: number) => CustomMatcherResult;
     }
