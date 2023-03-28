31533b3b4e14334dacda49fec274b4b69bbddf9a
melancholiai <samit2310@gmail.com>
2023-03-28 12:46:59 +0300

    deps: updated typeorm version due to bug in migration image

--- src/common/utils/retryFunctionWrapper.ts
+++ src/common/utils/retryFunctionWrapper.ts
@@ -6,14 +6,14 @@ interface RetryOptions<E extends constructor<Error>> {
   retryErrorType: E;
 }
 
-export const retryFunctionWrapper = async <R, A extends unknown[], F extends (...args: A) => Promise<R>, E extends constructor<Error>>(
+export const retryFunctionWrapper = async <A extends unknown[], F extends (...args: A) => Awaited<unknown>, E extends constructor<Error>>(
   options: RetryOptions<E>,
   func: F,
   ...args: A
 ): Promise<ReturnType<F>> => {
   for (let i = 0; i <= options.numberOfRetries; i++) {
     try {
-      return await func(...args);
+      return (await func(...args)) as ReturnType<F>;
     } catch (error) {
       if (!(error instanceof options.retryErrorType)) {
         throw error;
